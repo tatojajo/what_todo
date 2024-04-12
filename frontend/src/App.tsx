4ab16e393f5db9765ee719062fe81e9ce3ce9002
tatojajo <tatojajanidze@gmail.com>
2024-04-12 22:34:51 +0200

    todo commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,22 @@
-import { useEffect } from "react";
-import { isUserAuthenticated } from "./helpers/auth";
 import { Route, Routes, useNavigate } from "react-router-dom";
 
 import "./App.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
+import { useEffect } from "react";
+import { isUserAuthenticated } from "./helpers/auth";
 
 function App() {
   const navigate = useNavigate();
-
   useEffect(() => {
-    const userIsAuthenticated = isUserAuthenticated()?.isAuth;
-    if (userIsAuthenticated) {
+    const isAuthenticated = isUserAuthenticated()?.isAuth;
+    const currentPath = window.location.pathname;
+
+    if (isAuthenticated && currentPath !== "/register") {
       navigate("/home");
     }
-  }, [navigate]);
+  }, []);
 
   return (
     <main>
