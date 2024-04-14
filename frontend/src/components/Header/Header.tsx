ce37b6a56423ef55a817982311fd4978a4ac79c6
tatojajo <tatojajanidze@gmail.com>
2024-04-14 23:49:07 +0200

    add favicon and avatar images, change navigation settings

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -77,9 +77,9 @@ function Header() {
           </Typography>
 
           <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
+            <Tooltip title="user settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                <Avatar alt="user avatar" src="/public/avatar.jpg" />
               </IconButton>
             </Tooltip>
             <Menu
